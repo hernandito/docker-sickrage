cd8e294f1aab0d29606a7e0b406773754ed5edd5
hernandito <archi@inch.com>
2015-10-18 12:44:40 -0400

    Update crontask.sh

--- init/crontask.sh
+++ init/crontask.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cp -R /config/sickrage-edits/gui/slick /app/sickrage/gui/
+
 crontab -l > mycron
 echo "0/5 * * * * cp -R /config/sickrage-edits/gui/slick /app/sickrage/gui/" >> mycron
 crontab mycron
