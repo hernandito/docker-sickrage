e1022f6525a4cb1b27b3c97106ccf62c2308fa81
hernandito <archi@inch.com>
2015-11-20 17:09:01 -0500

    Update 30_update.sh

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage
+[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
 cd /app/sickrage
 git pull
 chown -R abc:abc /app /config
