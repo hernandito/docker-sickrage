a19dbd8ea3e342909923fe3516ef5ed8939a33c2
hernandito <archi@inch.com>
2015-11-28 16:58:56 -0500

    Update 30_update.sh

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
-[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/hernandito/SickRage.git /app/sickrage
+[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
+#[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/hernandito/SickRage.git /app/sickrage
 
 
 #[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage
