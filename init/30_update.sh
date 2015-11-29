dfdcebaeb930ec8bf3ce99e69c80f564622666bc
hernandito <archi@inch.com>
2015-11-29 00:00:57 -0500

    Update 30_update.sh

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
+#[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
 #[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/hernandito/SickRage.git /app/sickrage
-
+[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage/tree/poster-popout.git /app/sickrage
 
 #[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage
 cd /app/sickrage
